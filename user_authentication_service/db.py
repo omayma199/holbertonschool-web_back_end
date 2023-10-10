441c91311507c995874e540a86a74300b7388606
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-10 12:13:52 +0100

    db

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 """DB module
 """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
 from user import User  # Assuming your User model is imported from user.py
 
 class DB:
     """DB class
     """
 
-    # ... (existing code)
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database.
