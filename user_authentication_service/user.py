11ebe5a02a26d2966aa5aefd2fdbb25f7716a6f0
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-10 11:37:49 +0100

    user

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -5,6 +5,7 @@ SQLAlchemy model named User
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+
 Base = declarative_base()
 
 class User(Base):
@@ -16,4 +17,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
