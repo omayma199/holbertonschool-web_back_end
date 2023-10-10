1116aa4131ab7fbb6739a2fc4c334a2821ef66f2
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-10 15:10:56 +0100

    authservice

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -125,4 +125,3 @@ def update_password():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-    
--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -110,4 +110,3 @@ class Auth:
             self._db.update_user(user.id, hashed_password=hashed_password)
             self._db.update_user(user.id, reset_token=None)
         return None
-    
