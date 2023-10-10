81b13ad890e5b9e005ac94d550fddc5472875589
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-10 15:03:16 +0100

    authservice

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,20 +5,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from user import Base, User
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm.exc import NoResultFound
-from typing import TypeVar
 
-from user import User, Base  # Assuming your User model is imported from user.py
 
 class DB:
     """DB class
     """
-
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine(
+            "sqlite:///a.db",
+            echo=False,
+            connect_args={"check_same_thread": False})
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,23 +34,43 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database.
-
-        Args:
-            email (str): The email of the user.
-            hashed_password (str): The hashed password of the user.
-
-        Returns:
-            User: The User object representing the newly added user.
+        """ Add user to the database
         """
-        # Create a new User object with the provided email and hashed_password
-        new_user = User(email=email, hashed_password=hashed_password)
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
 
-        # Add the new_user to the session (but not the database yet)
-        self._session.add(new_user)
+    def find_user_by(self, **kwargs) -> User:
+        """ Find user by key_pairs arguements
+        """
+        if not kwargs or not self.valid_query_args(**kwargs):
+            raise InvalidRequestError
 
-        # Commit the session to persist the new_user to the database
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
+        else:
+            return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user based on user ID
+        """
+        if not self.valid_query_args(**kwargs):
+            raise ValueError
+
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            setattr(user, k, v)
+
         self._session.commit()
 
-        # Return the User object representing the newly added user
-        return new_user
+    def valid_query_args(self, **kwargs):
+        """Get users table columns
+        """
+        columns = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in columns:
+                return False
+        return True
