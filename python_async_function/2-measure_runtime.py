9ccf4ec7266041ab9915217eb3f85b739dbf18ea
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-11 14:27:16 +0100

    async_function

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -13,14 +13,13 @@ def measure_time(n: int, max_delay: int = 10) -> float:
     Measure the runtime
     """
     start_time = time()
-    
+ 
     # Measure the total execution time for wait_n(n, max_delay)
     wait_n(n, max_delay)
-    
+
     end_time = time()
-    
+
     # Calculate the total time and return the average time per iteration
     total_time = end_time - start_time
     average_time_per_iteration = total_time / n
     return average_time_per_iteration
-
