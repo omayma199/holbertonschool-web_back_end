afdc3a249f2a40e052201c150bc6fb8681596fe8
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-11 13:41:27 +0100

    python-variable-annotation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,7 +9,7 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Converts a string and an int/float to a tuple with 
+    Converts a string and an int/float to a tuple with
     the string and the square of the int/float as a float.
     """
     squared_value = float(v) ** 2
