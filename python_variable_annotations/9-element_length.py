6b77e4a99a7065499582a02b08ce33f3a4fdf9e8
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-11 13:50:19 +0100

    python-variable-annotation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,8 +4,9 @@ type an iterable object
 """
 
 
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
+
