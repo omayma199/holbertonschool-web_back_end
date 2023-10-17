6eef245f2b4adfa0d7990f18f826a4a834eb9ebd
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-17 14:19:08 +0100

    newproject

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,9 @@
 
 
 import unittest
+from unittest.mock import patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json, memoize
 
 class TestAccessNestedMap(unittest.TestCase):
 
@@ -23,10 +24,57 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",), "KeyError: 'a' not found in the nested map"),
         ({"a": 1}, ("a", "b"), "KeyError: 'b' not found in the nested map"),
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
+    def test_access_nested_map_exception(self, nested_map, path, result, expected_result):
+        """ Test method raises correct exception """
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(context.exception), expected_exception_message)
+            self.assertEqual(result, expected_result)
 
+
+class TestGetJson(TestCase):
+    """ Class for testing get_json function """
+    # order of args: test_url, test_payload
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """ Test method returns correct output """
+        # set mock response to have return value of test payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        # function calls requests.get, need patch to get mock return value
+        with patch('requests.get', return_value=mock_response):
+            real_response = get_json(test_url)
+            self.assertEqual(real_response, test_payload)
+            # check that mocked method called once per input
+            mock_response.json.assert_called_once()
+
+
+class TestMemoize(TestCase):
+    """ Class for testing memoization """
+
+    def test_memoize(self):
+        """ Tests memoize function """
+
+        class TestClass:
+            """ Test class """
+
+            def a_method(self):
+                """ Method to always return 42 """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ Returns memoized property """
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as patched:
+            test_class = TestClass()
+            real_return = test_class.a_property
+            real_return = test_class.a_property
+
+            self.assertEqual(real_return, 42)
+            patched.assert_called_once()
 if __name__ == '__main__':
     unittest.main()
