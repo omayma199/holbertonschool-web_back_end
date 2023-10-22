7cc832ab049d0b82ddd9e665d47721fa7e4a108e
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-22 12:03:51 +0100

    flask

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -8,10 +8,14 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
+    """
+    """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 
