8bb1257d733c04f8f731aa61ad56053f9fa60822
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-25 22:47:29 +0100

    pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -8,6 +8,8 @@ from typing import List, Tuple
 
 
 index_range = __import__('0-simple_helper_function').index_range
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -26,7 +28,7 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
-    
+
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         ''' Return page of dataset. '''
         assert isinstance(page, int) and isinstance(page_size, int)
