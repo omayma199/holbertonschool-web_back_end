456838d19fdb23be8d92b9eb918802893910dddb
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-25 22:29:17 +0100

    pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -27,17 +27,6 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            pass
-    
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """Calculate the start index"""
-        start_index = (page - 1) * page_size
-
-        """Calculate the end index"""
-        end_index = start_index + page_size
-
-        return (start_index, end_index)
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
         get page Simple pagination
         """
@@ -47,4 +36,14 @@ class Server:
         start, end = self.index_range(page, page_size)
         if end > len(dataset):
             return []
-        return [list(dataset[row]) for row in range(start, end)]
+        return [list(dataset[row]) for row in range(start, end)]        
+    
+    
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate the start index"""
+        start_index = (page - 1) * page_size
+
+        """Calculate the end index"""
+        end_index = start_index + page_size
+
+        return (start_index, end_index)
