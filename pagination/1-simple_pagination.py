a1353a58461f6a37e8f1691ed10c199c9ebf83ff
oumaimamizouni <mizounioumaima@gmail.com>
2023-10-25 22:26:23 +0100

    pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -2,10 +2,10 @@
 """
 Simple helper function
 """
-
 import csv
 import math
-from typing import List
+from typing import List, Tuple
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -27,27 +27,24 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        # Verify that both arguments are integers greater than 0
-        if not (isinstance(page, int) and isinstance(page_size, int) and page > 0 and page_size > 0):
+            pass
+    
+    def index_range(page: int, page_size: int) -> Tuple[int, int]:
+        """Calculate the start index"""
+        start_index = (page - 1) * page_size
+
+        """Calculate the end index"""
+        end_index = start_index + page_size
+
+        return (start_index, end_index)
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        get page Simple pagination
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        dataset = self.dataset()
+        start, end = self.index_range(page, page_size)
+        if end > len(dataset):
             return []
-
-        # Get the total number of rows in the dataset
-        total_rows = len(self.dataset())
-
-        # Calculate the start and end indexes using the index_range function
-        start_index, end_index = index_range(page, page_size)
-
-        # Check if the page is out of range
-        if start_index >= total_rows:
-            return []
-
-        # Slice the dataset to get the appropriate page
-        page_data = self.dataset()[start_index:end_index]
-
-        return page_data
-
-# Define the index_range function here
-def index_range(page, page_size):
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+        return [list(dataset[row]) for row in range(start, end)]
