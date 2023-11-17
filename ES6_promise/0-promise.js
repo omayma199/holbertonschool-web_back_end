bfe6a797b1121ccee350888173ea20f4766c3d7f
oumaimamizouni <mizounioumaima@gmail.com>
2023-11-17 21:23:11 +0100

    js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,9 +1,9 @@
 export default function getResponseFromAPI() {
-    // Assuming some asynchronous operation, like fetching data from an API
-    return new Promise((resolve) => {
-      // Simulating a successful API response after 1 second
-      setTimeout(() => {
-        resolve("API response data");
-      }, 1000);
-    });
-  }
+  // Assuming some asynchronous operation, like fetching data from an API
+  return new Promise((resolve) => {
+    // Simulating a successful API response after 1 second
+    setTimeout(() => {
+      resolve("API response data");
+    }, 1000);
+  });
+}
