6f05963a04b8ab681df82c167c56742064440ce9
oumaimamizouni <mizounioumaima@gmail.com>
2023-12-20 14:15:05 +0100

    data manipulation

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,8 @@
-function getListStudents() {
-  return [
+export default function getListStudents() {
+  const obj = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
+  return obj;
 }
