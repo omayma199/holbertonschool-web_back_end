8a287b03b6e4ed29c3d55c6dedbed1e52bc03843
mizouni <5752@holbertonstudents.com>
2024-01-04 14:26:42 +0100

    list student function

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,4 @@
-function getListStudentIds(students) {
-    // Check if the input is an array
-    if (!Array.isArray(students)) {
-      return [];
-    }
-
-    // Use the map function to extract ids from the array of objects
-    const ids = students.map(student => student.id);
-    return ids;
+export default function getListStudentIds(array) {
+    if (!Array.isArray(array)) return [];
+    return array.map((i) => i.id);
   }
