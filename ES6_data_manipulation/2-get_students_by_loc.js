4016edc5335920586e45229ce79a259b7f7cf863
mizouni <5752@holbertonstudents.com>
2024-01-04 14:32:01 +0100

    list student function

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,3 @@
-function getStudentsByLocation(students, city) {
-  // Use the filter function to get students located in the specified city
-  const studentsInCity = students.filter(student => student.location === city);
-  return studentsInCity;
+export default function getStudentsByLocation(array, city) {
+  return array.filter((i) => i.location === city);
 }
